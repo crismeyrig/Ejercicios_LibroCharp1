6090216436a7bf421a822893fba2aad041db9cff
Crismeyri Gutierrez Gil <crismeyri_gutierrez@ucne.edu.do>
2020-09-29 09:10:23 -0400

    Creando  las clases

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios_LibroCharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
